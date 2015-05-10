af246bdb4f651606ace594f836ca4d20edc45409
Norman Atashbar <natashbar@atlassian.com>
2015-05-11 00:02:30 +1000

    Week 3 finished

--- reactive-programming-2/week3-nodescala/src/main/scala/nodescala/package.scala
+++ reactive-programming-2/week3-nodescala/src/main/scala/nodescala/package.scala
@@ -94,7 +94,12 @@ package object nodescala {
      *  However, it is also non-deterministic -- it may throw or return a value
      *  depending on the current state of the `Future`.
      */
-    def now: T = ???
+    def now: T = f.value match {
+      case Some(Success(x)) => x
+      case Some(Failure(x)) => throw x
+      case _ => throw new NoSuchElementException()
+    }
+
 
     /** Continues the computation of this future by taking the current future
      *  and mapping it into another future.
@@ -102,7 +107,14 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continueWith[S](cont: Future[T] => S): Future[S] = ???
+    def continueWith[S](cont: Future[T] => S): Future[S] = {
+      val p = Promise[S]
+      f.onComplete {
+        case Failure(x) => p.failure(x)
+        case Success(x) => p.complete(Try(cont(Future.always(x))))
+      }   
+      p.future
+    }
 
     /** Continues the computation of this future by taking the result
      *  of the current future and mapping it into another future.
@@ -110,7 +122,11 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continue[S](cont: Try[T] => S): Future[S] = ???
+    def continue[S](cont: Try[T] => S): Future[S] = {
+      val p = Promise[S]
+      f.onComplete { x => p.complete(Try(cont(x))) }   
+      p.future
+    }
 
   }
 
