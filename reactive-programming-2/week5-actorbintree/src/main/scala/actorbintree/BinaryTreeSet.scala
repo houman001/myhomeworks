16463aa896b54728364331833a413f2eb8af0d46
Norman Atashbar <natashbar@atlassian.com>
2015-05-25 00:18:52 +1000

    Homework week 5

--- reactive-programming-2/week5-actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
+++ reactive-programming-2/week5-actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
@@ -66,15 +66,36 @@ class BinaryTreeSet extends Actor {
 
   // optional
   /** Accepts `Operation` and `GC` messages. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = { 
+    case o:Operation => root ! o
+    case GC => {
+      val newRoot = createRoot
+      context become garbageCollecting(newRoot)
+      root ! CopyTo(newRoot)
+    }    
+  }
 
   // optional
-  /** Handles messages while garbage collection is performed.
-    * `newRoot` is the root of the new binary tree where we want to copy
-    * all non-removed elements into.
-    */
-  def garbageCollecting(newRoot: ActorRef): Receive = ???
-
+  /**
+   * Handles messages while garbage collection is performed.
+   * `newRoot` is the root of the new binary tree where we want to copy
+   * all non-removed elements into.
+   */
+  def garbageCollecting(newRoot: ActorRef): Receive = {
+    case o: Operation => pendingQueue = pendingQueue enqueue o
+    case CopyFinished => {
+      context.stop(root)
+      root = newRoot
+      while (!pendingQueue.isEmpty) {
+        pendingQueue = pendingQueue.dequeue match {
+          case (o, q) =>
+            root ! o
+            q
+        }
+      }
+      context unbecome ()
+    }
+  }
 }
 
 object BinaryTreeNode {
@@ -101,13 +122,77 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor {
 
   // optional
   /** Handles `Operation` messages and `CopyTo` requests. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = { 
+    case Insert(requester, id, e) => insert(requester, id, e)
+    case Contains(requester, id, e) => contains(requester, id, e)
+    case Remove(requester, id, e) => remove(requester, id, e)
+    case CopyTo(node) => {
+      val expected = subtrees.values.toSet
+      context become copying(expected, false)
+      if(!removed)
+        node ! Insert(self, elem, elem)
+      else
+        self ! OperationFinished(elem)
+      expected.foreach(_ ! CopyTo(node))
+    }    
+  }
 
   // optional
-  /** `expected` is the set of ActorRefs whose replies we are waiting for,
-    * `insertConfirmed` tracks whether the copy of this node to the new tree has been confirmed.
-    */
-  def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = ???
+  /**
+   * `expected` is the set of ActorRefs whose replies we are waiting for,
+   * `insertConfirmed` tracks whether the copy of this node to the new tree has been confirmed.
+   */
+  def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = {
+    case OperationFinished(elem) =>
+      context become copying(expected, true)
+      if (expected.isEmpty)
+        self ! CopyFinished
 
+    case CopyFinished =>
+      val rest = expected - sender
+      if (rest.isEmpty && insertConfirmed) {
+        context.parent ! CopyFinished
+      } else {
+        context become copying(rest, insertConfirmed)
+      }
+  }
 
+  private def findSubtree(e: Int) = {
+    if (e < elem) Left else Right
+  }
+
+  private def insert(requester: ActorRef, id: Int, e: Int): Unit = {
+    if (e == elem) {
+      if (removed)
+        removed = false
+      requester ! OperationFinished(id)
+    } else
+      insert(findSubtree(e), requester, id, e)
+  }
+
+  private def insert(position: Position, requester: ActorRef, id: Int, e: Int): Unit = {
+    subtrees.get(position).map(_ ! Insert(requester, id, e)).getOrElse {
+      subtrees += (position -> context.actorOf(props(e, false)))
+      requester ! OperationFinished(id)
+    }
+  }
+
+  private def contains(requester: ActorRef, id: Int, e: Int): Unit = {
+    if (e == elem)
+      requester ! ContainsResult(id, !removed)
+    else
+      subtrees.get(findSubtree(e)).map(_ ! Contains(requester, id, e)).getOrElse {
+        requester ! ContainsResult(id, false)
+      }
+  }
+
+  private def remove(requester: ActorRef, id: Int, e: Int): Unit = {
+    if (e == elem) {
+      removed = true
+      requester ! OperationFinished(id)
+    } else
+      subtrees.get(findSubtree(e)).map(_ ! Remove(requester, id, e)).getOrElse {
+        requester ! OperationFinished(id)
+      }
+  }
 }
