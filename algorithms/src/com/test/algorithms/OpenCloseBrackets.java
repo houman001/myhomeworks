a43e3d5fb9a4cba22b603d4f7a63d3154c35058c
Norman Atashbar <norman@atashbar.com>
2016-05-02 09:37:08 +1000

    Measure timing for open close brackets

--- algorithms/src/com/test/algorithms/OpenCloseBrackets.java
+++ algorithms/src/com/test/algorithms/OpenCloseBrackets.java
@@ -1,23 +1,23 @@
 package com.test.algorithms;
 
 public class OpenCloseBrackets {
-    private static void printBrackets(int length) {
-        printBrackets("", length, length);
+    private static void printBrackets(int length, boolean print) {
+        printBrackets("", length, length, print);
     }
 
-    private static void printBrackets(String str, int openBrackets, int closeBrackets) {
+    private static void printBrackets(String str, int openBrackets, int closeBrackets, boolean print) {
         if (openBrackets < 0 || closeBrackets < 0) {
             System.err.println("INVALID STATE!");
         } else if (openBrackets == 0 && closeBrackets == 0) {
-            if (checkString(str)) {
+            if (checkString(str) && print) {
                 System.out.println(str);
             }
         } else {
             if (closeBrackets > 0 && closeBrackets > openBrackets) {
-                printBrackets(str + ")", openBrackets, closeBrackets - 1);
+                printBrackets(str + ")", openBrackets, closeBrackets - 1, print);
             }
             if (openBrackets > 0) {
-                printBrackets(str + "(", openBrackets - 1, closeBrackets);
+                printBrackets(str + "(", openBrackets - 1, closeBrackets, print);
             }
         }
     }
@@ -42,6 +42,11 @@ public class OpenCloseBrackets {
     }
 
     public static void main(String[] args) {
-        printBrackets(5);
+        for (int length = 1; length < 20; length++) {
+            long start = System.currentTimeMillis();
+            printBrackets(length, false);
+            long end = System.currentTimeMillis();
+            System.out.println("Time to process for " + length + ":" + (end - start));
+        }
     }
 }
