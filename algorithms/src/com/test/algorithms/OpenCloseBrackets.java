ebc0ff792e9d8e9b1886c018cc13d76b690625c9
Norman Atashbar <norman@atashbar.com>
2016-05-02 09:32:25 +1000

    Check open close brackets

--- algorithms/src/com/test/algorithms/OpenCloseBrackets.java
+++ algorithms/src/com/test/algorithms/OpenCloseBrackets.java
@@ -7,9 +7,11 @@ public class OpenCloseBrackets {
 
     private static void printBrackets(String str, int openBrackets, int closeBrackets) {
         if (openBrackets < 0 || closeBrackets < 0) {
-            System.out.println("INVALID STATE!");
+            System.err.println("INVALID STATE!");
         } else if (openBrackets == 0 && closeBrackets == 0) {
-            System.out.println(str);
+            if (checkString(str)) {
+                System.out.println(str);
+            }
         } else {
             if (closeBrackets > 0 && closeBrackets > openBrackets) {
                 printBrackets(str + ")", openBrackets, closeBrackets - 1);
@@ -18,6 +20,25 @@ public class OpenCloseBrackets {
                 printBrackets(str + "(", openBrackets - 1, closeBrackets);
             }
         }
+    }
+
+    private static boolean checkString(String str) {
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '(') {
+                sum++;
+            } else if (c == ')') {
+                sum--;
+            } else {
+                System.err.println("INVALID CHARACTER: " + c);
+            }
+            if (sum < 0) {
+                System.err.println("INVALID STRING GENERATED: " + str);
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
