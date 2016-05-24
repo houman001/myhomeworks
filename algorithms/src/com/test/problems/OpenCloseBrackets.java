ea4b9323fc243206af28b067d80dc16f09e2153e
Norman Atashbar <norman@atashbar.com>
2016-05-24 23:55:13 +1000

    Update open close brackets

--- algorithms/src/com/test/problems/OpenCloseBrackets.java
+++ algorithms/src/com/test/problems/OpenCloseBrackets.java
@@ -1,33 +1,48 @@
 package com.test.problems;
 
+import java.util.Stack;
+
 public class OpenCloseBrackets {
     private static void printBrackets(int length, boolean print) {
-        printBrackets("", length, length, print);
+        printBrackets("", 2 * length, 0, print);
     }
 
-    private static void printBrackets(String str, int openBrackets, int closeBrackets, boolean print) {
-        if (openBrackets < 0 || closeBrackets < 0) {
+    private static void printBrackets(String str, int remainingLength, int balance, boolean print) {
+        if (remainingLength < balance || balance < 0) {
             System.err.println("INVALID STATE!");
-        } else if (openBrackets == 0 && closeBrackets == 0) {
-            if (checkString(str) && print) {
+        } else if (remainingLength == 0) {
+            boolean check = checkStringUsingCount(str);
+            // boolean check = checkStringUsingStack(str);
+            if (check && print) {
                 System.out.println(str);
             }
         } else {
-            if (closeBrackets > 0 && closeBrackets > openBrackets) {
-                printBrackets(str + ")", openBrackets, closeBrackets - 1, print);
+            if (remainingLength > balance) {
+                printBrackets(str + "(", remainingLength - 1, balance + 1, print);
             }
-            if (openBrackets > 0) {
-                printBrackets(str + "(", openBrackets - 1, closeBrackets, print);
+            if (balance > 0) {
+                printBrackets(str + ")", remainingLength - 1, balance - 1, print);
             }
         }
     }
 
     private static boolean checkStringUsingStack(String str) {
-        // FIXME: Implement
-        return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.empty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.empty();
     }
 
-    private static boolean checkString(String str) {
+    private static boolean checkStringUsingCount(String str) {
         int sum = 0;
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
@@ -47,7 +62,7 @@ public class OpenCloseBrackets {
     }
 
     public static void main(String[] args) {
-        for (int length = 1; length < 20; length++) {
+        for (int length = 1; length < 10; length++) {
             long start = System.currentTimeMillis();
             printBrackets(length, false);
             long end = System.currentTimeMillis();
