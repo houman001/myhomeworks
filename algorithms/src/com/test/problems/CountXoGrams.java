755d120a38706c9b934aae38bbe54699bc2f0578
Norman Atashbar <norman@atashbar.com>
2016-06-02 19:44:57 +1000

    Count XoGrams

---
+++ algorithms/src/com/test/problems/CountXoGrams.java
@@ -0,0 +1,90 @@
+package com.test.problems;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class CountXoGrams {
+    private static Map<String, Integer> countMonograms(String text) {
+        return countGrams(text, 1);
+    }
+
+    private static Map<String, Integer> countBigrams(String text) {
+        return countGrams(text, 2);
+    }
+
+    private static Map<String, Integer> countTrigrams(String text) {
+        return countGrams(text, 3);
+    }
+
+    private static boolean isValid(String text) {
+        for (int i = 0; i < text.length(); i++) {
+            if (!Character.isAlphabetic(text.codePointAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static Map<String, Integer> countGrams(String text, int gramLength) {
+        Map<String, Integer> counts = new HashMap<>();
+        for (int i = 0; i <= text.length() - gramLength; i++) {
+            String subString = text.substring(i, i + gramLength).toLowerCase();
+            if (!isValid(subString)) {
+                continue;
+            }
+            int count = counts.containsKey(subString) ? counts.get(subString) : 0;
+            counts.put(subString, count + 1);
+        }
+        return counts;
+    }
+
+    private static Stream<Map.Entry<String, Integer>> sortByValue(Map<String, Integer> map) {
+        return map.entrySet().stream().sorted(Map.Entry.comparingByValue(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        }));
+    }
+
+    private static void printTop(Map<String, Integer> map) {
+        sortByValue(map).limit(5).forEachOrdered(e -> System.out.println(e.getKey() + ": " + e.getValue()));
+    }
+
+    public static void main(String[] args) {
+        // Check  results against http://practicalcryptography.com/cryptanalysis/text-characterisation/monogram-bigram-and-trigram-frequency-counts/
+        String sampleText = "How do I love thee? Let me count the ways.\n"
+                            + "\n"
+                            + "I love thee to the depth and breadth and height\n"
+                            + "\n"
+                            + "My soul can reach, when feeling out of sight\n"
+                            + "\n"
+                            + "For the ends of Being and ideal Grace\n"
+                            + "\n"
+                            + "I love thee to the level of everyday's\n"
+                            + "\n"
+                            + "Most quiet need, by sun and candle-light.\n"
+                            + "\n"
+                            + "I love thee freely, as men strive for Right;\n"
+                            + "\n"
+                            + "I love thee purely, as they turn from Praise.\n"
+                            + "\n"
+                            + "I love thee with the passion put to use\n"
+                            + "\n"
+                            + "In my old griefs, and with my childhood's faith\n"
+                            + "\n"
+                            + "I love thee with a love I seem to love\n"
+                            + "\n"
+                            + "With my lost saints, - I love thee with the breath,\n"
+                            + "\n"
+                            + "Smiles, tears, of all my life! - and, if God choose,\n"
+                            + "\n"
+                            + "I shall but love thee better after death.\n"
+                            + "\n";
+        printTop(countMonograms(sampleText));
+        printTop(countBigrams(sampleText));
+        printTop(countTrigrams(sampleText));
+    }
+}
--- algorithms/src/com/test/problems/MaxSumInArray.java
+++ algorithms/src/com/test/problems/MaxSumInArray.java
@@ -1,14 +1,87 @@
 package com.test.problems;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class MaxSumInArray {
-    private static void findMaxSubArrayRecursively(int[] array) {
-        // FIXME: Implement
+    private static void findMaxSubArray(int[] array) {
+        long startTime = System.currentTimeMillis();
+        MaxSubArray max = findMaxSubArrayRecursively(array, 0, array.length);
+        long now = System.currentTimeMillis();
+        long timeToProcess = now - startTime;
+        System.out.println(
+                "RECURSIVE: From: " + max.startIndex + ", to: " + max.endIndex + ", sum: " + max.sum + ", time: " +
+                timeToProcess);
+    }
+
+    private static MaxSubArray findMaxSubArrayRecursively(int[] array, int left, int right) {
+        if (left == right) {
+            return new MaxSubArray(left, right, Integer.MIN_VALUE);
+        } else if (left == right - 1) {
+            return new MaxSubArray(left, right, array[left]);
+        }
+        int mid = left + (right - left) / 2;
+        MaxSubArray crossingResult = findMaxCrossingSubArrayRecursively(array, left, right, mid);
+        MaxSubArray leftResult = findMaxSubArrayRecursively(array, left, mid);
+        MaxSubArray rightResult = findMaxSubArrayRecursively(array, mid, right);
+        if (crossingResult.sum > leftResult.sum && crossingResult.sum > rightResult.sum) {
+            return crossingResult;
+        }
+        if (leftResult.sum > rightResult.sum) {
+            return leftResult;
+        }
+        return rightResult;
+    }
+
+    private static MaxSubArray findMaxCrossingSubArrayRecursively(int[] array, int left, int right, int mid) {
+        if (left >= right || mid < left || mid > right) {
+            System.err.println("Invalid parameters calling findMaxSubArrayRecursively");
+            return new MaxSubArray(left, right, Integer.MIN_VALUE);
+        }
+        int maxLeftSum = Integer.MIN_VALUE;
+        int maxLeftIndex = mid;
+        int sum = 0;
+        for (int i = mid - 1; i >= left; i--) {
+            sum += array[i];
+            if (sum > maxLeftSum) {
+                maxLeftSum = sum;
+                maxLeftIndex = i;
+            }
+        }
+        int maxRightSum = Integer.MIN_VALUE;
+        int maxRightIndex = mid;
+        sum = 0;
+        for (int i = mid; i < right; i++) {
+            sum += array[i];
+            if (sum > maxRightSum) {
+                maxRightSum = sum;
+                maxRightIndex = i + 1;
+            }
+        }
+        int crossingSum = Integer.MIN_VALUE;
+        if (maxLeftSum > Integer.MIN_VALUE || maxRightSum > Integer.MIN_VALUE) {
+            if (maxLeftSum > Integer.MIN_VALUE && maxRightSum > Integer.MIN_VALUE) {
+                crossingSum = maxLeftSum + maxRightSum;
+            } else {
+                crossingSum = Math.max(maxLeftSum, maxRightSum);
+            }
+        }
+        return new MaxSubArray(maxLeftIndex, maxRightIndex, crossingSum);
+    }
+
+    private static class MaxSubArray {
+        int startIndex;
+        int endIndex;
+        int sum;
+
+        private MaxSubArray(int startIndex, int endIndex, int sum) {
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
+            this.sum = sum;
+        }
     }
 
     private static void findMaxSubArrayInLinearTime(int[] array) {
+        long startTime = System.currentTimeMillis();
         int maxSubStartingIndex = 0;
         int maxSubEndingIndex = 0;
         int maxSubSum = Integer.MIN_VALUE;
@@ -28,18 +101,23 @@ public class MaxSumInArray {
                 maxSubSum = endingMaxSubSum;
             }
         }
-        System.out.println("From: " + maxSubStartingIndex + ", to: " + maxSubEndingIndex + ", sum: " + maxSubSum);
+        long now = System.currentTimeMillis();
+        long timeToProcess = now - startTime;
+        System.out.println(
+                "LINEAR: From: " + maxSubStartingIndex + ", to: " + maxSubEndingIndex + ", sum: " + maxSubSum +
+                ", time: " + timeToProcess);
     }
 
     public static void main(String[] args) {
-        int TEST_ARRAY_LENGTH = 16;
+        int TEST_ARRAY_LENGTH = 10000000;
         int MIN_RANDOM_NUMBER = -10;
         int MAX_RANDOM_NUMBER = 10;
         int[] array = new int[TEST_ARRAY_LENGTH];
         for (int i = 0; i < TEST_ARRAY_LENGTH; i++) {
             array[i] = new Random().nextInt(MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER + 1) + MIN_RANDOM_NUMBER;
         }
-        System.out.println("Array: " + Arrays.toString(array));
+        // System.out.println("Array: " + Arrays.toString(array));
+        findMaxSubArray(array);
         findMaxSubArrayInLinearTime(array);
         int sum = 0;
         int[] sumArray = new int[TEST_ARRAY_LENGTH];
@@ -47,6 +125,6 @@ public class MaxSumInArray {
             sum += array[i];
             sumArray[i] = sum;
         }
-        System.out.println("Sum: " + Arrays.toString(sumArray));
+        // System.out.println("Sum: " + Arrays.toString(sumArray));
     }
 }
